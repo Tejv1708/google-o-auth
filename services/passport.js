b05568f653cb9a7e2693563d9c23c65c9524a362
Tejv1708 <narukatejveer@gmail.com>
2024-08-15 11:27:04 +0530

    third commit

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ console.log("google client id : " , process.env.googleClientID)
 passport.use(new GoogleStrategy({
     clientID : process.env.googleClientID_prod,
     clientSecret : process.env.googleClientSecret_prod,
-    callbackURL : '/auth/google/callback'
+    callbackURL : 'https://google-o-auth.onrender.com/auth/google/callback'
 } ,  (accessToken , refreshToken , profile , done) => {
     User.findOne({googleId : profile.id}).then((existingUser) => {
    if(existingUser){
